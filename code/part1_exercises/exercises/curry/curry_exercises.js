1e5566857110e9f01374b6e3990ba5551707fa63
William Horton <wdt.horton@gmail.com>
2017-02-17 11:40:02 -0500

    Complete some exercises

--- code/part1_exercises/exercises/curry/curry_exercises.js
+++ code/part1_exercises/exercises/curry/curry_exercises.js
@@ -6,24 +6,22 @@ var _ = require('ramda');
 //==============
 // Refactor to remove all arguments by partially applying the function
 
-var words = function(str) {
-  return split(' ', str);
-};
+var words = split(' ');
 
 // Exercise 1a
 //==============
 // Use map to make a new words fn that works on an array of strings.
 
-var sentences = undefined;
+var sentences = map(words);
 
 
 // Exercise 2
 //==============
 // Refactor to remove all arguments by partially applying the functions
 
-var filterQs = function(xs) {
-  return filter(function(x){ return match(/q/i, x);  }, xs);
-};
+var filterQs = filter(match(/q/i));
+
+
 
 
 // Exercise 3
@@ -34,24 +32,22 @@ var filterQs = function(xs) {
 var _keepHighest = function(x,y){ return x >= y ? x : y; };
 
 // REFACTOR THIS ONE:
-var max = function(xs) {
-  return reduce(function(acc, x){
-    return _keepHighest(acc, x);
-  }, 0, xs);
-};
+var max = reduce(_keepHighest, 0);
 
-  
+
 // Bonus 1:
 // ============
 // wrap array's slice to be functional and curried.
 // //[1,2,3].slice(0, 2)
-var slice = undefined;
+var slice = curry(function(start, end, xs) {
+  return xs.slice(start, end);
+});
 
 
 // Bonus 2:
 // ============
 // use slice to define a function "take" that takes n elements. Make it curried
-var take = undefined;
+var take = slice(0);
 
 
 module.exports = { words: words,
